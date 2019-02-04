939ad4f73c47c13cc30d0e50c680bfd7082900a3
milllante <47297516+milllante@users.noreply.github.com>
2019-02-04 14:24:01 +0300

    Update settings.py

--- myblog/settings.py
+++ myblog/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
-"""
+
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -125,4 +125,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = '/staticR'
-"""
+
