4abe220dab5af7434d3047e80eaf27c3a1122766
Milllante <milllante13@gmail.com>
2019-08-19 11:21:37 +0300

    Add pr1

--- myblog/settings.py
+++ myblog/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = '+ni6j*1qnkrf+$ey=kujfl(u&@us4mpkaty_)0wju0p7=d-l54'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '.elenaohota.pythonanywhere.com',
+    '.elenaohota.pythonanywhere.com.'
+]
 
 # Application definition
 
